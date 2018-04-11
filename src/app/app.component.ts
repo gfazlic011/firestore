43ebd19183476ce91f4233e1ca5c65e03a96406f
romulus0305 <fazlic.mr.goran@gmail.com>
2018-04-11 14:02:12 +0200

    Dodaj update metodu za sada radi

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,83 @@
 import { Component } from '@angular/core';
+import { AngularFirestore, AngularFirestoreCollection, AngularFirestoreDocument } from 'angularfire2/firestore';
+import { Observable } from 'rxjs/Observable';
+import 'rxjs/add/operator/map';
+
+interface Post{
+  title:string;
+  content:string;
+}
+
+interface PostId extends Post {
+  id:string;
+}
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
+
 export class AppComponent {
-  title = 'app';
+  
+  postsCollection: AngularFirestoreCollection<Post>;
+  // posts: Observable<Post[]>;
+  posts: any
+  title: string;
+  content: string;
+  Id:string
+  postDoc:AngularFirestoreDocument<Post>;
+  post: Observable<Post>;
+
+
+  constructor(private angularFirestore:AngularFirestore) {}
+  
+  addPost(){
+    this.angularFirestore.collection('posts').add({'title':this.title,'content':this.content})
+    // this.angularFirestore.collection('posts').doc('custom_id').set({'title':this.title,'content':this.content})
+  }
+
+  getPost(postId){
+    this.postDoc = this.angularFirestore.doc('posts/'+postId);
+    this.post = this.postDoc.valueChanges();
+  }
+
+  updatePost(postId){
+    
+    //napravi update post 
+
+  }
+
+
+  ngOnInit(){
+
+    this.postsCollection = this.angularFirestore.collection('posts');
+    // this.posts = this.postsCollection.valueChanges();
+    this.posts = this.postsCollection.snapshotChanges()
+    .map(action => {
+      return action.map(a =>{
+        const data = a.payload.doc.data() as Post;
+        const id = a.payload.doc.id;
+
+        return {id,data};
+      })
+      
+    })
+
+  }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,8 +1,20 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { AngularFireModule } from 'angularfire2';
+import { AngularFirestoreModule } from 'angularfire2/firestore';
+import { FormsModule } from "@angular/forms";
 
 import { AppComponent } from './app.component';
+
+//promenjeno
+var firebaseConfig = {
+  apiKey: "AIzaSyAMozwXSC2CQkoBJe0YCCtql_X3TnFRmeQ",
+  authDomain: "firestore-xigo011.firebaseapp.com",
+  databaseURL: "https://firestore-xigo011.firebaseio.com",
+  projectId: "firestore-xigo011",
+  storageBucket: "firestore-xigo011.appspot.com",
+  messagingSenderId: "978125356065"
+};
 
 
 @NgModule({
@@ -10,7 +22,10 @@ import { AppComponent } from './app.component';
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    AngularFireModule.initializeApp(firebaseConfig),  
+    AngularFirestoreModule, 
+    FormsModule                           
   ],
   providers: [],
   bootstrap: [AppComponent]
